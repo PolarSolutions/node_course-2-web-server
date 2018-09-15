348dedf8cc566bfd6b9f5b87b3106799761ee990
Rafael Polanco <rafaelpolancoe@gmail.com>
2018-09-15 01:41:53 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const hbs = require("hbs");
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render("about.hbs");
+});
+
+app.use(express.static("public"));
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
+
+app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    name: "Rafael1",
+    likes: ["Singing", "Computer"],
+    pageTitle: "Home page Mustache"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About page Mustache"
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    errorMessage: "Bad request"
+  });
+});
+
+app.listen(3000);
